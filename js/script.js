7a3a5e4741f54c80cefd55d6289181cc631973a4
hiroyukigs <taro.osg@gsmail.com>
2020-06-13 18:49:34 +0900

    コード修正

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const puchi_normal = [
   'img02.svg',
   'img03.svg'
 ];
-
+const $crash01 = $('.crash01').get(0);
 const $audio1 = $('.audio1').get(0);
 const $audio2 = $('.audio2').get(0);
 const $audio3 = $('.audio3').get(0);
@@ -26,11 +26,32 @@ function audio_random() {
   }
 }
 
-let count = 0;
-function puchi_count() {
-  count++;
-  $('.puchi_count span').text(count);
+let score = 0;
+function puchi_score() {
+  score++;
+  $('.puchi_score span').text(score);
 }
+
+
+
+
+
+
+$('.crash_btn').on('click', function () {
+  $crash01.play();
+  $('.puchi_puchi_list').addClass('action');
+  $('.puchi_puchi_crash').addClass('action');
+  $('.crash_btn').addClass('action');
+
+  score += 25;
+  $('.puchi_score span').text(score);
+  setTimeout(function () {
+    $('.puchi_puchi_list').removeClass('action');
+    $('.puchi_puchi_crash').removeClass('action');
+    $('.crash_btn').removeClass('action');
+  }, 1500);
+
+});
 
 for (let i = -1; ++i < 25;) {
   const puchi_normal_path = puchi_normal[Math.floor(Math.random() * puchi_normal.length)];
@@ -39,8 +60,8 @@ for (let i = -1; ++i < 25;) {
   $('.puchi_puchi_' + i).on('click', function () {
     const puchi_break_path = puchi_break[Math.floor(Math.random() * puchi_break.length)];
     $('.puchi_puchi_' + i).html('<span style="background-image:url(img/puchi_break/' + puchi_break_path + ' );"></span>');
-    audio_random()
-    puchi_count();
+    audio_random();
+    puchi_score();
     $('.puchi_puchi_' + i).addClass('none');
     setTimeout(function () {
       const puchi_break_path = puchi_normal[Math.floor(Math.random() * puchi_normal.length)];
@@ -50,8 +71,55 @@ for (let i = -1; ++i < 25;) {
   });
 }
 
-$('.Twitter_btn').click(function () {
-  var tw_contents = ("合計" + count + "ぷちぷち");
+
+
+
+
+
+
+//shop
+$('.shop_btn').on('click', function () {
+  $('.shop_content').addClass('open');
+});
+$('.shop_content .close').on('click', function () {
+  $('.shop_content').removeClass('open');
+  $('.buy_thanks').hide();
+  $('.buy_not').hide();
+  $('.buy_btn').show();
+
+
+});
+$('.crash_btn').hide();
+$('.buy_thanks').hide();
+$('.buy_not').hide();
+
+$('.buy_btn').on('click', function () {
+
+
+  if (score < 20) {
+
+    $('.buy_not').show();
+    $('.buy_btn').hide();
+
+
+  } else {
+    $('.buy_thanks').show();
+    $('.buy_btn').hide();
+    $('.shop_btn').hide();
+    $('.crash_btn').show();
+    score -= 20;
+    $('.puchi_score span').text(score);
+
+
+
+  }
+
+});
+
+//Twitter
+
+$('.twitter_btn').click(function () {
+  var tw_contents = ("合計" + score + "ぷちぷち");
   var url = "https://puchi.hryk-n.com";
   window.open().location.href = ("https://twitter.com/share?url=" + url + "&text=" + tw_contents + "&hashtags=むげんぷちぷち&count=none&lang=ja");
 });
